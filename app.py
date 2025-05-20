2a6f38a81136d542469597c77757b1950daed14c
jesus <xesulopez@gmail.com>
2025-05-20 19:03:54 +0200

    k
    jhk

--- app.py
+++ app.py
@@ -7,25 +7,5 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
-
-@app.route('/contact')
-def contact():
-    return render_template('contact.html')
-
-@app.route('/events/spain')
-def event_spain():
-    return render_template('event_spain.html')
-
-@app.route('/events/monaco')
-def event_monaco():
-    return render_template('event_monaco.html')
-
-@app.route('/events/canada')
-def event_canada():
-    return render_template('event_canada.html')
-
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
